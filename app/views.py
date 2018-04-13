ac22ca100ea12cfa18bfd986fcfb9062c6b650c7
QwertygidQ <Qwertygid@gmail.com>
2018-04-13 20:43:48 +0300

    Buttons update

--- app/views.py
+++ app/views.py
@@ -1,26 +1,29 @@
 from . import app
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request, abort
 from .models import Fit
 from .forms import DataForm, FitSearchForm
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     fileform = DataForm()
     searchform = FitSearchForm()
 
-    if fileform.data_submit.data and fileform.validate_on_submit():
+    if request.form.get('data-submit') == 'Upload file' and fileform.validate_on_submit():
         return 'file sent'
-
-    elif searchform.fit_search_submit.data and searchform.validate_on_submit():
-        return redirect(url_for('show_fit', id=searchform.fit_id))
+    elif request.form.get('data-submit') == 'Search' and searchform.validate_on_submit():
+        return redirect(url_for('show_fit', id=searchform.fit_id.data))
+    else:
+        print(fileform.errors)
+        print(searchform.errors)
 
     return render_template('index.html',
-                            fileform=fileform,
-                            searchform=searchform,
-                            title='Main Page')
+                           fileform=fileform,
+                           searchform=searchform,
+                           title='Main Page')
 
-@app.route('/fit/<id>')
+
+@app.route('/fit/<id>', methods=['GET', 'POST'])
 def show_fit(id):
     fit = Fit.query.filter_by(id=id).first()
 
@@ -28,13 +31,13 @@ def show_fit(id):
         abort(404)
 
     return render_template(
-                'fit.html',
-                fit=fit,
-                title='Fit #'+str(fit.id)
-            )
+        'fit.html',
+        fit=fit,
+        title='Fit #' + str(fit.id)
+    )
+
 
 @app.route('/418')
 def about():
     flash('418 I am a teapot')
     return redirect(url_for('index'))
-
