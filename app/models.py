a0b3a30afaf7e386860e5d3ed967a7fc0576e2ef
syn <isaqtm@gmail.com>
2018-04-13 15:20:27 +0300

    view for fit

--- app/models.py
+++ app/models.py
@@ -3,5 +3,9 @@ from app import app, db
 class Fit(db.Model):
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    done = db.Column(db.Boolean)
+    filename = db.Column(db.Text)
 
+    def status(self):
+        return 'OK'
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,27 @@
 from . import app
 from flask import render_template
+from .models import Fit
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/fit/<id>')
+def show_fit(id):
+    
+    fit = Fit.query.filter_by(id=id).first()
+
+    if not fit:
+        abort(404)
+
+    return render_template(
+                'fit.html',
+                fit=fit
+                id=id
+            )
+
+@app.route('/418')
+def about():
+    return '418 I am teapot'
 
