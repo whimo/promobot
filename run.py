bc552cb5c799df397129fa3efcb956bfb5e9531a
syn <isaqtm@gmail.com>
2018-04-13 14:39:32 +0300

    WSGI support

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from app import app, db
+from app import app as application
 import config
 import sys
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1].isdigit() and 1 < int(sys.argv[1]) < 65535:
         config.port = int(sys.argv[1])
-    app.run(host=config.host, port=config.port, debug=config.debug)
+    application.run(host=config.host, port=config.port, debug=config.debug)
 
