dfc6b0e264eadd6e8d8fff6ced062c874848ae44
syn <isaqtm@gmail.com>
2018-04-13 21:09:36 +0300

    General improvements

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
-from wtforms import IntegerField, SubmitField
+from wtforms import IntegerField, SubmitField, RadioField, DecimalField, SelectMultipleField, FieldList, FormField
 
 
 class DataForm(FlaskForm):
@@ -58,3 +58,26 @@ _brands = ['Proglide',
            'Venus 2',
            'Venus 3',
            'Wipes']
+
+
+_promo_types = ['Display',
+                'Display&Feature',
+                'Mega Event',
+                'TPR',
+                'Display&Banner',
+                'None']
+
+
+class EntryForm(FlaskForm):
+    budget_from = DecimalField()
+    budget_to = DecimalField()
+    promo_type = RadioField(_promo_types[0], choices=[(t, t) for t in _promo_types])
+    brand = SelectMultipleField(_brands[0], choices=[(b, b) for b in _brands])
+    repeat_count = IntegerField()
+
+
+class GetPredictForm(FlaskForm):
+    category = RadioField(_categories[0], choices=[(cat, cat) for cat in _categories])
+    num_of_entries = IntegerField()
+    promo_budget = DecimalField()
+    per_entry_params = FieldList(FormField())
