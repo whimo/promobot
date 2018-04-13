196e712d2f591ed844f885f09517053ca0cfb36e
syn <isaqtm@gmail.com>
2018-04-13 16:46:57 +0300

    Basic forms for index

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm, IntegerField
-from flask_wtf.file import FileForm, FileRequired
+from flask_wtf.file import FileForm, FileRequired, IntegerField
 
 
 class DataForm(FlaskForm):
     file_data = FileField(validators=[FileRequired()])
+
+
+class FitSearchForm(FlaskForm):
+    fit_id = IntegerField()
 
 
 _categories = ['Shave Prep',
@@ -18,8 +22,36 @@ _categories = ['Shave Prep',
                'Surface Care',
                'Wipes']
 
-
-class GetPredictForm(FlaskForm):
-    category = RadioField(u'Category', choices=[(cat, cat) for cat in _categories])
-    num_of_entries = IntegerField(validators=[Required()])
-
+_brands = ['Proglide',
+           'Always',
+           'Ariel',
+           'Aussie',
+           'BAM',
+           'Discreet',
+           'Fairy',
+           'Fusion',
+           'Gillette',
+           'Head & Shoulders',
+           'Lenor',
+           'Mach3',
+           'Myth',
+           'Naturella',
+           'Old Spice',
+           'Oral-B',
+           'Pantene',
+           'Mr. Proper',
+           'Series',
+           'Tampax',
+           'Tide',
+           'Venus',
+           'Wella Pro',
+           'Blue 2',
+           'Blue 3',
+           'Gillette 2',
+           'Gillette APDO',
+           'Satin Care',
+           'Safeguard',
+           'Slalom',
+           'Venus 2',
+           'Venus 3',
+           'Wipes']
