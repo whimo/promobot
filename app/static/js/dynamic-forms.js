439f05a0709f961d91e2c34204970c953f9bbbaa
QwertygidQ <Qwertygid@gmail.com>
2018-04-14 11:34:05 +0300

    Saving

---
+++ app/static/js/dynamic-forms.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $base_form = $(".dynamically-repeated").first();
+    $append_div = $("#dynamic");
+    
+    
+    
+    //$base_form.clone().appendTo($append_div)
+})
